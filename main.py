81ba5dadfeccf35dd011a7c46f232f69e9c93c79
London Lowmanstone <lowmanstone@college.harvard.edu>
2018-12-08 23:34:40 -0500

    about to test

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import rnn
 
 # def main(train_model, generate_song, data_file, ckpt_file):
-training_name = "test_train"
-rnn.main(train_model=True, generate_song=True, data_file="London/LondonData/{}.txt".format(training_name),
-         ckpt_file="London/LondonModels/{}/model.ckpt".format(training_name), song_file="London/LondonSongs/{}.txt".format(training_name))
+training_name = "rep_tweets"
+data_folder = "data"
+models_folder = "models"
+new_tweets_folder = "tweets"
+rnn.main(train_model=True, generate_song=True,
+         data_file="{}/{}.txt".format(data_folder, training_name),
+         ckpt_file="{}/{}/model.ckpt".format(models_folder, training_name),
+         song_file="{}/{}.txt".format(new_tweets_folder, training_name))
