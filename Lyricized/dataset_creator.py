a72cdbb39a55381710ef84df3a96ee3add289420
London Lowmanstone <lowmanstone@college.harvard.edu>
2018-12-08 23:20:48 -0500

    dataset creator works

---
+++ Lyricized/dataset_creator.py
@@ -0,0 +1,21 @@
+import csv
+with open("dem_tweets.csv", "w+") as dem_save_file:
+    with open("rep_tweets.csv", "w+") as rep_save_file:
+        with open('tweets.csv', newline='') as csv_file:
+            spamreader = csv.reader(csv_file, delimiter=',')
+            for row_index, row in enumerate(spamreader):
+                print(row_index)
+                if len(row) > 2:
+                    file_to_write_to = None
+                    if row[1] == "HillaryClinton":
+                        file_to_write_to = dem_save_file
+                    elif row[1] == "realDonaldTrump":
+                        file_to_write_to = rep_save_file
+                    else:
+                        print("Unrecognized handle: {}".format(row[1]))
+                    if file_to_write_to:
+                        file_to_write_to.write(row[2])
+                        file_to_write_to.write("\n\n\n")
+# 0 gives ID
+# 1 gives name
+# 2 gives tweet
